e7f2729d1c6ba1228853e4628234743ca6afe6ac
Aamod Pisat <aamod.pisat@raweng.com>
2016-07-05 18:59:30 +0530

    hooks

--- plugins/blog-app/hooks/index.js
+++ plugins/blog-app/hooks/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by Aamod Pisat on 04-07-2016.
  */
+module.exports = function (app , Blog) {
+    app.use('/', function (req, res, next) {
+        var options= {};
+        Blog.getCategories(options)
+            .spread(function success(entries) {
+                next();
+            });
+    });
+};
--- plugins/blog-app/index.js
+++ plugins/blog-app/index.js
@@ -6,6 +6,8 @@
  * Module dependencies
  */
 var contentstack =  require('contentstack-express'),
+    Blog= require('./models/blog'),
+    Hooks = require('./hooks/index'),
     Home = require('./routes/home'),
     Category = require('./routes/category'),
     Author = require('./routes/author'),
@@ -14,6 +16,7 @@ module.exports = function BlogApp() {
     var options = BlogApp.options,
         baseRoute =  options.baseRoute || '/';
     BlogApp.serverExtends = function(app) {
+        Hooks(app, Blog); // To attach data model with routes
         app.use(baseRoute,Home); // Home Route
         app.use(baseRoute,Category); // Category route
         app.use(baseRoute,Author); // Author Route
