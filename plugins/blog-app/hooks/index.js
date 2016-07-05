1065a8614b219fade15c453993fa1d75a0933cb2
Aamod Pisat <aamod.pisat@raweng.com>
2016-07-05 19:19:09 +0530

    hooks

--- plugins/blog-app/hooks/index.js
+++ plugins/blog-app/hooks/index.js
@@ -2,6 +2,9 @@
  * Created by Aamod Pisat on 04-07-2016.
  */
 module.exports = function (app , Blog) {
+    /*
+       To add data model to your routes
+     */
     app.use('/', function (req, res, next) {
         var options= {};
         Blog.getCategories(options)
