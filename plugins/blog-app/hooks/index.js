209d0a00ff8ff4d000860f3d66f450e4d8f6b9d9
Aamod Pisat <aamod.pisat@raweng.com>
2016-07-07 12:34:22 +0530

    hooks

--- plugins/blog-app/hooks/index.js
+++ plugins/blog-app/hooks/index.js
@@ -1,11 +1,12 @@
 /**
  * Created by Aamod Pisat on 04-07-2016.
  */
-module.exports = function (app , Blog) {
+var Blog = require('./../models/blog');
+module.exports = function(app) {
     /*
      * To add data model to your routes
      */
-    app.use('/', function (req, res, next) {
+    app.use('/', function(req, res, next) {
         var options= {};
         Blog.getCategories(options)
             .spread(function success(entries) {
--- plugins/blog-app/index.js
+++ plugins/blog-app/index.js
@@ -6,8 +6,6 @@
  * Module dependencies
  */
 var contentstack =  require('contentstack-express'),
-    Blog= require('./models/blog'),
-    Hooks = require('./hooks/index'),
     Home = require('./routes/home'),
     Category = require('./routes/category'),
     Author = require('./routes/author'),
@@ -16,7 +14,7 @@ module.exports = function BlogApp() {
     var options = BlogApp.options,
         baseRoute =  options.baseRoute || '/';
     BlogApp.serverExtends = function(app) {
-        Hooks(app, Blog); // To attach data model with routes
+        var Hooks = require('./hooks/index')(app); // To attach data model with routes
         app.use(baseRoute,Home); // Home Route
         app.use(baseRoute,Category); // Category route
         app.use(baseRoute,Author); // Author Route
